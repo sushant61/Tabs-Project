c980ddacc8bd4656d8f78192c36c8fff054e54b3
Sushant Prasad <sushantprasad1234@gmail.com>
2022-09-08 22:56:33 +0530

    commitedt

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+const url = "https://course-api.com/react-tabs-project";
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setvalue] = useState(0);
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  if (isLoading)
+    return (
+      <section className="section-loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  const { company, title, dates, duties } = jobs[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.div}
+                className={`job-btn ${index === value && "active-btn"}`}
+                onClick={() => setvalue(index)}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="jobs-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+      <button className="btn">more info</button>
+    </section>
+  );
+}
+export default App;
